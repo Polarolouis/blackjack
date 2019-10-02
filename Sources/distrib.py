e5adf88b54d3ec73d712491b1386248a6f839062
ellerom <julienmorelle@hotmail.fr>
2019-10-02 14:47:33 +0200

    Create distrib.py

---
+++ Sources/distrib.py
@@ -0,0 +1,5 @@
+def distrib(LJ,J,P):
+    if len(P) % len(LJ) # verif qu'on peut jouer
+    for c in range(2):
+        for i in LJ:
+            J[i][1].append(P.pop()) # On ajoute une carte dans la liste de carte du joueur i
