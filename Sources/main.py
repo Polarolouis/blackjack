bacb1527e4bed05657b49162470daa1ac8410d0a
LordOf20th <lordof20th@hotmail.fr>
2019-11-13 13:04:03 +0100

    Rennomage des as

--- Sources/main.py
+++ Sources/main.py
@@ -16,7 +16,7 @@ def initialisation(n): # Création de la liste des joueurs, et le dictionnaire d
             LJ.append("Joueur "+str(i))
     J = {}
     for i in LJ:
-        J[i] = [0, stratAlea(), [], 200]  # [mise, stratégie, main, capital]
+        J[i] = [0, stratAlea(), [], 0, 200]  # [mise, stratégie, main, valeur main, capital]
     return LJ,J
 
 #Mélange
@@ -24,8 +24,8 @@ def melange(n):
     """ Fonction générant un mélange de plusieurs paquets de cartes
     sous la forme d'une liste """
     P = n*52*[0]
-    conv = ['1H', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', '1S', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', '1D', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', '1C', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC']
-    dic = {'1H': n, '2H': n, '3H': n, '4H': n, '5H': n, '6H': n, '7H': n, '8H': n, '9H': n, '10H': n, 'JH': n, 'QH': n, 'KH': n, '1S': n, '2S': n, '3S': n, '4S': n, '5S': n, '6S': n, '7S': n, '8S': n, '9S': n, '10S': n, 'JS': n, 'QS': n, 'KS': n, '1D': n, '2D': n, '3D': n, '4D': n, '5D': n, '6D': n, '7D': n, '8D': n, '9D': n, '10D': n, 'JD': n, 'QD': n, 'KD': n, '1C': n, '2C': n, '3C': n, '4C': n, '5C': n, '6C': n, '7C': n, '8C': n, '9C': n, '10C': n, 'JC': n, 'QC': n, 'KC': n}
+    conv = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC']
+    dic = {'AH': n, '2H': n, '3H': n, '4H': n, '5H': n, '6H': n, '7H': n, '8H': n, '9H': n, '10H': n, 'JH': n, 'QH': n, 'KH': n, 'AS': n, '2S': n, '3S': n, '4S': n, '5S': n, '6S': n, '7S': n, '8S': n, '9S': n, '10S': n, 'JS': n, 'QS': n, 'KS': n, 'AD': n, '2D': n, '3D': n, '4D': n, '5D': n, '6D': n, '7D': n, '8D': n, '9D': n, '10D': n, 'JD': n, 'QD': n, 'KD': n, 'AC': n, '2C': n, '3C': n, '4C': n, '5C': n, '6C': n, '7C': n, '8C': n, '9C': n, '10C': n, 'JC': n, 'QC': n, 'KC': n}
     for i in range((n*52)):         # on affecte à chaque valeur de la liste P un nom de carte
         while P[i] == 0:            # tant que le iéme terme de la liste n'a pas de nom on va continuer a essayer de lui en trouver                                             une disponible
             r = randint(0, 51)      # on tire aléatoirement une carte parmis les 52 d'un paquet
@@ -36,10 +36,8 @@ def melange(n):
 
 # Distribution
 def testJouable(LJ, J, P):
-    if len(P) >= 2*len(LJ):  # verif qu'on peut jouer
+    if len(P) > 52:  # verif qu'on peut jouer
         return True
-    else:
-        return False
 
 def distrib(LJ,J,P,n):  # LJ : Liste du nom index des joueurs | J : dictionnaire avec en index les { noms index des joueurs : [Main du joueur] }
         for c in range(n):
@@ -80,6 +78,8 @@ def main(nbreJoueurs, nbrePCartes, strat):
 
         """ Distribution des mains """
         distrib(listeJoueurs, infoJoueurs, P, 2)
+        vmain = valeurMain(infoJoueurs[i][3])
+        infoJoueurs[i][4] = vmain
 
         """ Deuxième phase de mise """
         phase = 2
