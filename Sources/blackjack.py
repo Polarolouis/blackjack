794574d9428b1521980ffcff9500699952525008
ellerom <julienmorelle@hotmail.fr>
2019-12-18 15:30:22 +0100

    Update blackjack.py
    ajout de stratbasique

--- Sources/blackjack.py
+++ Sources/blackjack.py
@@ -172,6 +172,39 @@ def stratAlea(phase, infosJoueurs, P, valUpCard):
     infosJoueurs[4] = capital
     infosJoueurs[3] = valeurMain(main)
 
+def stratbasique(phase, infosJoueurs, P, valUpCard):
+    # avec phase qui correspond à la phase de jeu
+    # On récupère les valeurs dont la carte du croupier (cartecroup)
+    cartecroup= valUpCard
+    capital = infosJoueurs[4]
+    mise = infosJoueurs[0]
+    main = infosJoueurs[2]
+
+    if phase == 1:  # Mise de départ
+        mise = randint(2, capital)
+        capital -= mise
+    elif phase == 2:
+        jouer = True
+        while jouer == True :
+            if valeurMain(main) <=8 or (valeurMain(main) == 9 and (cartecroup>=7 or cartecroup==2)) or (valeurMain(main)==10 and cartecroup>=10) or (valeurMain(main)== 12 and (cartecroup==2 or cartecroup==3)) or ((valeurMain(main)>= 12 and valeurMain(main)<=16) and cartecroup>=7):
+                main.append(tirerUneCarte(P)) # On ajoute une carte à la main
+                # il va hit
+            elif valeurMain(main) >= 17 or ((valeurMain(main)>= 13 and valeurMain(main) <= 16) and cartecroup <=6) or (valeurMain(main) == 12 and (cartecroup>=4 and cartecroup <=6)):
+                jouer = False
+                # il va stand
+            else:
+                main.append(tirerUneCarte(P)) # On ajoute une carte à la main
+                capital -= mise
+                mise = 2*mise
+                jouer= False
+                # il va double
+
+    # On attribue les nouvelles valeurs
+    infosJoueurs[2] = main
+    infosJoueurs[0] = mise
+    infosJoueurs[4] = capital
+    infosJoueurs[3] = valeurMain(main)
+
 def principale(nbreJoueurs, nbrePCartes, strat):
     tour = 0
     listeJoueurs, infoJoueurs = initialisation(nbreJoueurs)
