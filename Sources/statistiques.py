2bbddfad33cb80ab776473e078e46b048d42fd3f
LordOf20th <lordof20th@hotmail.fr>
2020-02-19 19:02:17 +0100

    Ajout d'un fichier statistiques pour faire des stats sur des grands nombres

---
+++ Sources/statistiques.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 18:58:13 2020
+
+@authors: Julien Morelle & Louis Lacoste
+"""
+import blackjack as bj
+
+print(bj.stat(bj.stratHiLow, 10000))
